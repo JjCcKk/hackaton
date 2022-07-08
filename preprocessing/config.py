47250838afe712ed417eb8275c868f318cdf17cf
sonicpierre <pvirgaux@gmail.com>
2022-07-08 21:33:17 +0200

    Simplification

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -1,9 +1,9 @@
 #Preprocessing configuration
 
-DATA_INI = "/home/virgaux/Hackathon/Sound_of_AI/hackaton/mmsd_raw_data/utterances_final/"
-JSON_DATA = "/home/virgaux/Hackathon/Sound_of_AI/hackaton/MUStARD/data/sarcasm_data.json"
-ANNOTATIONS_FILE_TRAIN = "train.csv"
-ANNOTATIONS_FILE_TEST = "test.csv"
+DATA_INI = "/inputs/videoinit/utterances_final/"
+JSON_DATA = "/inputs/repo/data/sarcasm_data.json"
+ANNOTATIONS_FILE_TRAIN = "/outputs/data/train.csv"
+ANNOTATIONS_FILE_TEST = "/outputs/data/test.csv"
 AUDIO_DIR = "/"
 SAMPLE_RATE = 44100
 #NUM_SAMPLES = 44100
