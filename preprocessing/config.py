e08904c36e1bedb8ccce89443e536cdfe0a2dfd4
sonicpierre <pvirgaux@gmail.com>
2022-07-08 18:58:53 +0200

    Simplification

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -1,9 +1,7 @@
-import os
-
 #Preprocessing configuration
 
-DATA_INI = os.getcwd() + "/mmsd_raw_data/utterances_final"
-JSON_DATA = os.getcwd() + "/MUStARD/data/sarcasm_data.json"
+DATA_INI = "/home/virgaux/Hackathon/Sound_of_AI/hackaton/mmsd_raw_data/utterances_final"
+JSON_DATA = "/home/virgaux/Hackathon/Sound_of_AI/hackaton/MUStARD/data/sarcasm_data.json"
 ANNOTATIONS_FILE_TRAIN = "train.csv"
 ANNOTATIONS_FILE_TEST = "test.csv"
 AUDIO_DIR = "/"
--- preprocessing/create_test_train.py
+++ preprocessing/create_test_train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from config import ANNOTATIONS_FILE_TRAIN, ANNOTATIONS_FILE_TEST, DATA_INI, JSON_DATA
 
 if __name__=="__main__":
+    print(JSON_DATA)
     data = pd.read_json(JSON_DATA).T
     data["Path"] = [DATA_INI + str(i)[0] + "_" + str(i)[1:] + ".mp4" for i in data.index]
     data.index = range(data.shape[0])
