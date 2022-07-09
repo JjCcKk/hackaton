72df221a14f06668ca7af8083bfe46b2ed94ee5e
sonicpierre <pvirgaux@gmail.com>
2022-07-09 20:44:34 +0200

    Simplification

--- production/frontend/app.py
+++ production/frontend/app.py
@@ -11,13 +11,7 @@ def greet(audio:tuple) -> str:
     y, sr = librosa.load("my_song.wav", sr=audio[0])
     mel_signal = librosa.feature.melspectrogram(y=y, sr=sr, hop_length=330, n_fft=32)
     spectrogram = np.abs(mel_signal)
-    power_to_db = librosa.power_to_db(spectrogram, ref=np.max)
-    librosa.display.specshow(power_to_db, sr=sr, x_axis='time', y_axis='mel', cmap='magma', hop_length=330)
-    plt.colorbar(label='dB')
-    plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=18))
-    plt.xlabel('Time', fontdict=dict(size=15))
-    plt.ylabel('Frequency', fontdict=dict(size=15))
-    plt.savefig("my_fig.png")
+    print(spectrogram)
     return "hello ma gueule"
 
 
