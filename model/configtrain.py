3d662fe2781ff282b397715ea00a5d917b1739fc
sonicpierre <pvirgaux@gmail.com>
2022-07-09 16:18:02 +0200

    Pipeline debugg

--- model/configtrain.py
+++ model/configtrain.py
@@ -1,4 +1,4 @@
 TRAIN_FILE = "/inputs/data/mel_spec_train/"
 TRAIN_ANNOTATION = "/inputs/data/annotation_train.json"
-TEST_FILE = "/inputs/data/mel_spec_train/"
+TEST_FILE = "/inputs/data/mel_spec_test/"
 TEST_ANNOTATION = "/inputs/data/annotation_test.json"
